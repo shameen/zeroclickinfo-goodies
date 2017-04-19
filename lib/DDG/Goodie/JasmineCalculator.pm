bf1b037d94707db579a4fc0e94903bddf130148b
Zaahir Moolla <moollaza@duckduckgo.com>
2017-04-19 13:04:37 -0400

    cleanup perl

--- lib/DDG/Goodie/JasmineCalculator.pm
+++ lib/DDG/Goodie/JasmineCalculator.pm
@@ -1,8 +1,4 @@
 package DDG::Goodie::JasmineCalculator;
-# ABSTRACT: Write an abstract here
-
-# Start at http://docs.duckduckhack.com/walkthroughs/calculation.html if
-# you are new to instant answer development
 
 use DDG::Goodie;
 use strict;
@@ -10,43 +6,18 @@ use warnings;
 
 zci answer_type => 'jasmine_calculator';
 
-# Caching - http://docs.duckduckhack.com/backend-reference/api-reference.html#caching`
 zci is_cached => 1;
 
-# Triggers - http://docs.duckduckhack.com/walkthroughs/calculation.html#triggers
 triggers any => 'triggerword', 'trigger phrase';
 
-# Handle statement
 handle remainder => sub {
 
     my $remainder = $_;
 
-    # Optional - Guard against no remainder
-    # I.E. the query is only 'triggerWord' or 'trigger phrase'
-    #
-    # return unless $remainder;
-
-    # Optional - Regular expression guard
-    # Use this approach to ensure the remainder matches a pattern
-    # I.E. it only contains letters, or numbers, or contains certain words
-    #
-    # return unless qr/^\w+|\d{5}$/;
-
     return 'plain text response',
         structured_answer => {
-
-            data => {
-                title    => 'My Instant Answer Title',
-                subtitle => 'My Subtitle',
-                # image => 'http://website.com/image.png',
-            },
-
-            templates => {
-                group => 'text',
-                # options => {
-                #
-                # }
-            }
+            data => 1,
+            templates => 1
         };
 };
 
